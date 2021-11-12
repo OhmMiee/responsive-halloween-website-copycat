df788a137cd068b225cc3d801ac5493c93a05748
Jeraphan Boonsin <jeraphan2000@gmail.com>
2021-11-13 01:13:09 +0700

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,21 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== HOME SWIPER ====================*/
+var swiper = new Swiper(".home-swiper", {
+    spaceBetween: 30,
+    loop: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+});
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
